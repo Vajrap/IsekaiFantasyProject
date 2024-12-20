e490a1a39e529041c815ebd9463b9bc21cf1ba8d
Vajrap <103096793+Vajrap@users.noreply.github.com>
2024-12-20 11:13:16 +0000

    Removed ununsed character objects

--- Server/Entities/Character/NPCS/NPCs.ts
+++
@@ -1,8 +0,0 @@
-// import { npc_karin } from "./allNpcCharacters/KarinFarsee";
-// import { npc_satoshi } from "./allNpcCharacters/SatoshiIkeda";
-
-// export const allNPCS = {
-//     npc_karin,
-//     npc_satoshi
-// }
-
